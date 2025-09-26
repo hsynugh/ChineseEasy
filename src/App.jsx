649f95c0f14dd9e9bbe96f5311eff2e7479f6f04
hsynugh <hsynalyafy888@gmail.com>
2025-09-27 00:16:12 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <h1 style={{color: 'white'}}>Welcome to ChineseEasy 👋</h1>
+    </div>
+  )
+}
